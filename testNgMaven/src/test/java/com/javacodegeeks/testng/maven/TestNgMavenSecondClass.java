636063f7486fbb91c7bad2b1393c47a9cf9bc5fe
Kiran Sharma <kiransharma755@hotmail.com>
2015-10-12 11:41:08 -0400

    the test project

---
+++ testNgMaven/src/test/java/com/javacodegeeks/testng/maven/TestNgMavenSecondClass.java
@@ -0,0 +1,15 @@
+package com.javacodegeeks.testng.maven;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenSecondClass {
+
+	/**
+	 * @param args
+	 */
+	@Test
+	public void oneMoreTest() {
+		System.out.println("This is a TestNG-Maven based test");
+	}
+
+}
